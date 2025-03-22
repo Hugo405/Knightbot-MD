bb218134822126efcd72f1f04f19ba47eb5035a7
Hugo405 <130311680+Hugo405@users.noreply.github.com>
2025-03-22 21:22:41 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Reaper',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Reaper Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '27631575701', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
