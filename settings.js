f50c4de916b9eae2286802ba7ac684d31d2e35df
Hugo405 <130311680+Hugo405@users.noreply.github.com>
2025-04-16 00:21:30 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Reaper',
-  author: 'Bot',
-  botName: "Reaper Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '27631575701', //Your number
+  packname: 'Optimus Prime',
+  author: 'Prime',
+  botName: "Prime Bot",
+  botOwner: 'Prime X', // Your name
+  ownerNumber: '27768255187', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
